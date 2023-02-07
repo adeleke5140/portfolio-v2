4ff9786abc487e54059584d2fe2585363e234eaa
Kenny <adelekekehinde06@gmail.com>
2023-02-07 03:02:23 +0100

    add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { satoshiFont, erodeFont } from "@/fonts/setup";
+import { Layout } from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <div
+        className={`${satoshiFont.variable} ${erodeFont.variable} home font-satoshi`}
+      >
+        <Component {...pageProps} />
+      </div>
+    </Layout>
+  );
 }
