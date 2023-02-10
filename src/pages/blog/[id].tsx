3cb799e6e68e4694d35cd2d92303c33edf3a4685
Kenny <adelekekehinde06@gmail.com>
2023-02-10 22:24:55 +0100

    create dynmaic component for posts

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,67 @@
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+} from "next";
+import { getAllPostsIds, getPostData } from "@/lib/posts";
+import { ParsedUrlQuery } from "querystring";
+import type { Post } from "./index";
+import Head from "next/head";
+import { format, parseISO } from "date-fns";
+import { PostWrapper } from "@/components/postWrapper";
+
+interface PostData extends Post {
+  contentHtml: string;
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+const Post = ({
+  postData,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  return (
+    <>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <PostWrapper path="/blog">
+        <section className="pb-8 mt-8">
+          <div className="font-erode">
+            <h1 className="font-extrabold mb-4 text-5xl">{postData.title}.</h1>
+            <p className="text-link-color">
+              {format(parseISO(postData.date!), "LLLL d, yyyy")}
+            </p>
+          </div>
+          <section
+            className="mt-12 font-satoshi prose"
+            dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          ></section>
+        </section>
+      </PostWrapper>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<{ postData: PostData }> = async ({
+  params,
+}) => {
+  const { id } = params as Params;
+  const postData = await getPostData(id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export default Post;
