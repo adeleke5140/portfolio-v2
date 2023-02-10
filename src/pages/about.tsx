6891d295d7362ffe71ed02881b9ed5a7ba7f044d
Kenny <adelekekehinde06@gmail.com>
2023-02-10 22:24:33 +0100

    use pagewrapper in about me

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { BackButton } from "@/components/backButton";
+import { PageWrapper } from "@/components/pageWrapper";
+
+const About = () => {
+  return (
+    <PageWrapper heading="About me." path="/">
+      about me
+    </PageWrapper>
+  );
+};
+
+export default About;
