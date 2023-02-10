3b3ff71d65f7cda8c66c86ffb41de9d25d2f0d1a
Kenny <adelekekehinde06@gmail.com>
2023-02-10 22:24:47 +0100

    add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import { BackButton } from "@/components/backButton";
+import { PageWrapper } from "@/components/pageWrapper";
+
+const Projects = () => {
+  return (
+    <PageWrapper heading="Projects." path="/">
+      <h2>projects</h2>
+    </PageWrapper>
+  );
+};
+
+export default Projects;
