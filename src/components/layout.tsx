adf46f040ca739a03ea46f2c19b417ac4cd72fcf
Kenny <adelekekehinde06@gmail.com>
2023-02-10 22:24:04 +0100

    update layout component file

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,17 +9,14 @@ const siteTitle = "Portfolio v2";
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="w-fullcontainer home">
+    <div className="home h-screen">
       <Head>
         <link rel="icon" href="/kehinde.ico" />
         <meta name="description" content="Kehinde Adeleke's website" />
         <meta name="og:title" content={siteTitle} />
       </Head>
       <section className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
-        <main className="h-screen pt-8 md:pt-16">{children}</main>
-        <footer>
-          <p>{siteTitle}</p>
-        </footer>
+        <main className="pt-8 md:pt-16">{children}</main>
       </section>
     </div>
   );
