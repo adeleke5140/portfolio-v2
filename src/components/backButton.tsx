b9efde7d185bb8f7ee2b045b8e42f6d1e008702a
Kenny <adelekekehinde06@gmail.com>
2023-02-10 22:26:32 +0100

    update svg property

--- src/components/backButton.tsx
+++ src/components/backButton.tsx
@@ -16,9 +16,9 @@ const BackButton = ({ path, text }: BackButtonProps) => {
             height="24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M19 12H5M12 19l-7-7 7-7"></path>
           </svg>
