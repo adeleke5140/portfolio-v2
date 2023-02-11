aadfc1aa5dca23c0d702a2e9f59d1d37bb549789
Kenny <adelekekehinde06@gmail.com>
2023-02-11 05:46:01 +0100

    add proper title to page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,8 @@
 import { PageWrapper } from "@/components/pageWrapper";
+import { formatDate } from "@/helpers/formatDate";
 import { getSortedPostsData } from "@/lib/posts";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import Link from "next/link";
 
 export type Post = {
@@ -12,19 +14,32 @@ const Index = ({
   allPostsData,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <PageWrapper heading="Blog." path="/">
-      <section className="mt-8 font-erode h-screen">
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              <Link href={`/blog/${id}`}>{title}</Link>
-              <br />
-              <span className="text-gray-500">{date}</span>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </PageWrapper>
+    <>
+      <Head>
+        <title>Kehinde | Blog </title>
+        <link rel="icon" href="/kehinde.ico" />
+        <meta name="description" content="Kehinde Adeleke's website" />
+        <meta name="og:title" content="Blog" />
+      </Head>
+      <PageWrapper heading="Blog." path="/">
+        <p className="text-lg font-erode font-bold mb-4">
+          A couple of my writings:{" "}
+        </p>
+        <section className="font-erode">
+          <ul>
+            {allPostsData.map(({ id, date, title }) => (
+              <li key={id} className="mb-2 font-satoshi">
+                <Link className="text-sm" href={`/blog/${id}`}>
+                  {title}
+                </Link>
+                <br />
+                <span className="text-gray-500">{formatDate(date!)}</span>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </PageWrapper>
+    </>
   );
 };
 
