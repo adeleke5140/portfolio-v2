cfd50822ad60b79d59ddceb537e1a820ad28a1ff
Kenny <adelekekehinde06@gmail.com>
2023-02-11 05:43:57 +0100

    update layout component styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,14 +9,14 @@ const siteTitle = "Portfolio v2";
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="home h-screen">
+    <div className="home min-h-screen">
       <Head>
         <link rel="icon" href="/kehinde.ico" />
         <meta name="description" content="Kehinde Adeleke's website" />
         <meta name="og:title" content={siteTitle} />
       </Head>
       <section className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
-        <main className="pt-8 md:pt-16">{children}</main>
+        <main className="pt-8 md:pt-12">{children}</main>
       </section>
     </div>
   );
