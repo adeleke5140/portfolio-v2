54b9e603603ad60653f6ebce408edf105a78f4ee
Kenny <adelekekehinde06@gmail.com>
2023-02-11 13:44:13 +0100

    style the projects card

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,17 +7,17 @@ const Projects = () => {
         <p className="mb-4">
           A couple of interesting things I have built include:
         </p>
-        <div className="rounded border border-link-color px-4 py-4 mb-4">
-          <div className="flex justify-between">
+        <section className="rounded border border-link-color px-4 py-4 mb-4 hover:bg-button-bg hover:bg-opacity-60 transition-transform ease-out duration-200 translate-x-1 hover:translate-x-0">
+          <div className="flex justify-between ">
             <div className="mb-2">
               <p className="font-bold">LiveChat</p>
               <span className="text-sm">A realtime message platform</span>
             </div>
-            <div className="flex gap-2 text-button-text">
+            <div className="flex gap-2 text-button-text ">
               <span>
                 <a
                   href="https://livechat-98e8c.web.app/"
-                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-button-bg -translate-x-1 hover:translate-x-0 rounded"
+                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-app-bg -translate-x-1 hover:translate-x-0 rounded"
                 >
                   Live
                 </a>
@@ -25,7 +25,7 @@ const Projects = () => {
               <span>
                 <a
                   href="https://github.com/adeleke5140/LiveChat"
-                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-button-bg -translate-x-1 hover:translate-x-0 rounded"
+                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-app-bg -translate-x-1 hover:translate-x-0 rounded"
                 >
                   Github
                 </a>
@@ -36,8 +36,8 @@ const Projects = () => {
             <span className="font-bold">Tools:</span> Vue, Firebase Auth,
             Firebase Firestore, CSS
           </span>
-        </div>
-        <div className="rounded border border-link-color px-4 py-4">
+        </section>
+        <section className="rounded border border-link-color px-4 py-4 hover:bg-button-bg hover:bg-opacity-60 transition-transform ease-out duration-200 translate-x-1 hover:translate-x-0">
           <div className="flex justify-between">
             <div className="mb-2">
               <p className="font-bold">CountryFact App</p>
@@ -49,7 +49,7 @@ const Projects = () => {
               <span>
                 <a
                   href="https://country-fact.netlify.app/"
-                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-button-bg -translate-x-1 hover:translate-x-0 rounded"
+                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-app-bg -translate-x-1 hover:translate-x-0 rounded"
                 >
                   Live
                 </a>
@@ -57,7 +57,7 @@ const Projects = () => {
               <span>
                 <a
                   href="https://github.com/adeleke5140/country-fact-project"
-                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-button-bg -translate-x-1 hover:translate-x-0 rounded"
+                  className="p-2 font-medium transition-transform ease-out duration-200 hover:bg-app-bg -translate-x-1 hover:translate-x-0 rounded"
                 >
                   Github
                 </a>
@@ -68,7 +68,7 @@ const Projects = () => {
             <span className="font-bold">Tools:</span> React, RestAPI, CSS,
             LocalStorage
           </span>
-        </div>
+        </section>
       </section>
     </PageWrapper>
   );
