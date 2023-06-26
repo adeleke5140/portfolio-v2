ce933d22424e3e356777e7833a02fb8b07eead90
Kenny <adelekekehinde06@gmail.com>
2023-06-26 14:12:26 +0100

    fix: center blog posts

--- src/components/postWrapper.tsx
+++ src/components/postWrapper.tsx
@@ -7,7 +7,7 @@ interface PostWrapperProps {
 
 const PostWrapper = ({ children, path }: PostWrapperProps) => {
   return (
-    <section className="h-full relative pr-8 pb-8 md:pb-0 md:pr-0">
+    <section className="h-full mx-auto w-full relative pr-8 pb-8 md:pb-0 md:pr-0">
       <div className="flex flex-col gap-5">{children}</div>
       <BackButton text="Blog" path={path} />
     </section>
