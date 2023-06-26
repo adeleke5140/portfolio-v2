9e14d6f9a44e66232b9b86fae6a0d8e9980e8170
Kenny <adelekekehinde06@gmail.com>
2023-06-26 14:10:23 +0100

    fix: replace section with div

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,9 +15,9 @@ const Layout = ({ children }: LayoutProps) => {
         <meta name="description" content="Kehinde Adeleke's website" />
         <meta name="og:title" content={siteTitle} />
       </Head>
-      <section className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
+      <div className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
         <main className="pt-8 md:pt-12">{children}</main>
-      </section>
+      </div>
     </div>
   );
 };
