102c99ae8183d98459859c0dfeefd9567c0fd100
Kenny <adelekekehinde06@gmail.com>
2023-07-05 00:56:08 +0100

    feat: update wrapper

--- src/components/pageWrapper.tsx
+++ src/components/pageWrapper.tsx
@@ -8,13 +8,15 @@ interface PageWrapperProps {
 
 const PageWrapper = ({ heading, children, path }: PageWrapperProps) => {
   return (
-    <section className="h-full relative pr-8">
-      <div className="flex flex-col gap-5 mb-8">
-        <h1 className="title font-satoshi">{heading}</h1>
-        <BackButton path={path} text="Home" />
-      </div>
-      {children}
-    </section>
+    <div className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
+      <section className="h-full relative pr-8">
+        <div className="flex flex-col gap-5 mb-8">
+          <h1 className="title font-satoshi">{heading}</h1>
+          <BackButton path={path} text="Home" />
+        </div>
+        {children}
+      </section>
+    </div>
   );
 };
 
