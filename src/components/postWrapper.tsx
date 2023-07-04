94f352851e932d5b4b931adc8d8e06528cb993ca
Kenny <adelekekehinde06@gmail.com>
2023-07-05 00:56:17 +0100

    feat: update container

--- src/components/postWrapper.tsx
+++ src/components/postWrapper.tsx
@@ -1,16 +1,15 @@
-import { BackButton } from "./backButton";
-
 interface PostWrapperProps {
   path: string;
   children: React.ReactNode;
 }
 
-const PostWrapper = ({ children, path }: PostWrapperProps) => {
+const PostWrapper = ({ children }: PostWrapperProps) => {
   return (
-    <section className="h-full mx-auto w-full relative pr-8 pb-8 md:pb-0 md:pr-0">
-      <div className="flex flex-col gap-5">{children}</div>
-      <BackButton text="Blog" path={path} />
-    </section>
+    <div className="max-w-xl mx-auto">
+      <section className="w-full pr-8 pb-8 md:pb-0 md:pr-0">
+        <div className="flex flex-col gap-5">{children}</div>
+      </section>
+    </div>
   );
 };
 
