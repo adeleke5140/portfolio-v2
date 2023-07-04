6149178bd7f8ea7bd3ded18abba66cc6f9b31fca
Kenny <adelekekehinde06@gmail.com>
2023-07-05 00:56:46 +0100

    feat: remove unnecessary component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { HoverCardComponent } from "@/components/info";
 import Head from "next/head";
 import Link from "next/link";
-import dynamic from "next/dynamic";
-
-const DynamicHoverComponent = dynamic(
-  () => import("../components/info").then((mod) => mod.HoverCardComponent),
-  {
-    ssr: false,
-  }
-);
 
 export default function Home() {
   return (
@@ -20,56 +11,45 @@ export default function Home() {
         <link rel="icon" href="/kehinde.ico" />
       </Head>
 
-      <section className="flex flex-col mt-[43%] justify-center" id="parent">
-        <header className="mb-8">
-          <h1 className="font-satoshi text-heading-color text-7xl sm:text-8xl font-extrabold">
-            Kehinde.
-          </h1>
-          <h2 className="mt-8 font-normal leading-[1.6] font-erode">
-            web engineer
-          </h2>
-        </header>
-        <nav>
-          <p>
-            <Link
-              href="/blog"
-              className="inline-block font-satoshi text-link-color text-3xl opacity-60 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-            >
-              /blog.
-            </Link>
-          </p>
-          <p className="mt-4">
-            <Link
-              href="/about"
-              className="inline-block font-satoshi text-link-color text-3xl opacity-60 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-            >
-              /about.
-            </Link>
-          </p>
-          <p className="mt-4">
-            <Link
-              href="/projects"
-              className="inline-block font-satoshi text-link-color text-3xl opacity-60 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-            >
-              /projects.
-            </Link>
-          </p>
-        </nav>
-        <div className="font-erode text-sm sm:text-base absolute bottom-0 pb-2">
-          <div className="hidden md:block ">
-            <DynamicHoverComponent />
-          </div>
-          <p className="block font-erode">
-            Built with 愛 by{" "}
-            <a
-              href="https://github.com/adeleke5140"
-              className="font-medium underline text-link-color"
-            >
-              adeleke5140
-            </a>{" "}
-          </p>
-        </div>
-      </section>
+      <div className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
+        <section className="flex flex-col mt-[43%] justify-center" id="parent">
+          <header className="mb-8">
+            <h1 className="font-satoshi text-heading-color text-7xl sm:text-8xl font-extrabold">
+              Kehinde.
+            </h1>
+            <h2 className="mt-8 font-normal leading-[1.6] font-erode">
+              web engineer
+            </h2>
+          </header>
+          <nav>
+            <p>
+              <Link
+                href="/blog"
+                className="inline-block font-satoshi text-link-color text-3xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
+              >
+                /blog.
+              </Link>
+            </p>
+            <p className="mt-4">
+              <Link
+                href="/about"
+                className="inline-block font-satoshi text-link-color text-3xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
+              >
+                /about.
+              </Link>
+            </p>
+            <p className="mt-4">
+              <Link
+                href="/projects"
+                className="inline-block font-satoshi text-link-color text-3xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
+              >
+                /projects.
+              </Link>
+            </p>
+          </nav>
+
+        </section>
+      </div>
     </>
   );
 }
