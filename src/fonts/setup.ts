97e7997c061e81ed45ab2ffcb62f2b92ce5e65d1
Kenny <adelekekehinde06@gmail.com>
2023-07-05 00:56:28 +0100

    feat: add font

--- src/fonts/setup.ts
+++ src/fonts/setup.ts
@@ -1,115 +1,128 @@
 import localFont from '@next/font/local'
+import { DM_Mono, DM_Sans } from '@next/font/google'
 
 export const satoshiFont = localFont({
-    src: [
-        {
-            path: './Satoshi/Satoshi-Light.otf',
-            weight: '300',
-            style: 'normal'
-        },
-        {
-            path: './Satoshi/Satoshi-LightItalic.otf',
-            weight: '300',
-            style: 'italic'
-        },
-        {
-            path: './Satoshi/Satoshi-Regular.otf',
-            weight: '400',
-            style: 'normal'
-        },
-        {
-            path: './Satoshi/Satoshi-Italic.otf',
-            weight: '400',
-            style: 'italic'
-        },
-        {
-            path: './Satoshi/Satoshi-Medium.otf',
-            weight: '500',
-            style: 'normal'
-        },
-        {
-            path: './Satoshi/Satoshi-MediumItalic.otf',
-            weight: '500',
-            style: 'italic'
-        },
-        {
-            path: './Satoshi/Satoshi-Bold.otf',
-            weight: '700',
-            style: 'normal'
-        },
-        {
-            path:'./Satoshi/Satoshi-BoldItalic.otf',
-            weight: '700',
-            style: 'italic'
-        },
-        {
-            path: './Satoshi/Satoshi-Black.otf',
-            weight: '900',
-            style: 'normal'
-        },
-        {
-            path: './Satoshi/Satoshi-BlackItalic.otf',
-            weight: '900',
-            style: 'italic'
-        }
-    ],
-    variable: "--font-satoshi",
-    display: 'block'
+  src: [
+    {
+      path: './Satoshi/Satoshi-Light.otf',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: './Satoshi/Satoshi-LightItalic.otf',
+      weight: '300',
+      style: 'italic'
+    },
+    {
+      path: './Satoshi/Satoshi-Regular.otf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './Satoshi/Satoshi-Italic.otf',
+      weight: '400',
+      style: 'italic'
+    },
+    {
+      path: './Satoshi/Satoshi-Medium.otf',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './Satoshi/Satoshi-MediumItalic.otf',
+      weight: '500',
+      style: 'italic'
+    },
+    {
+      path: './Satoshi/Satoshi-Bold.otf',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: './Satoshi/Satoshi-BoldItalic.otf',
+      weight: '700',
+      style: 'italic'
+    },
+    {
+      path: './Satoshi/Satoshi-Black.otf',
+      weight: '900',
+      style: 'normal'
+    },
+    {
+      path: './Satoshi/Satoshi-BlackItalic.otf',
+      weight: '900',
+      style: 'italic'
+    }
+  ],
+  variable: "--font-satoshi",
+  display: 'block'
 })
 
 export const erodeFont = localFont({
-    src: [
-        {
-            path:'./Erode/Erode-Light.otf',
-            weight:'300',
-            style:"normal"
-        },
-        {
-            path:'./Erode/Erode-LightItalic.otf',
-            weight:'300',
-            style:"italic"
-        },
-        {
-            path: './Erode/Erode-Regular.otf',
-            weight:'400',
-            style:"normal"
-        },
-        {
-            path:'./Erode/Erode-Italic.otf',
-            weight:"400",
-            style:"italic"
-        },
-        {
-            path:'./Erode/Erode-Medium.otf',
-            weight:"500",
-            style:"normal"
-        },
-        {
-            path:'./Erode/Erode-MediumItalic.otf',
-            weight:"500",
-            style:"italic"
-        },
-        {
-            path:'./Erode/Erode-Semibold.otf',
-            weight:"600",
-            style:"normal"
-        },
-        {
-            path:'./Erode/Erode-SemiboldItalic.otf',
-            weight:"600",
-            style:"italic"
-        },
-        {
-            path:'./Erode/Erode-Bold.otf',
-            weight:"700",
-            style:"normal",
-        },
-        {
-            path: './Erode/Erode-BoldItalic.otf',
-            weight:"700",
-            style:"italic",
-        }
-    ],
-    variable: "--font-erode",
-    display: 'block'
+  src: [
+    {
+      path: './Erode/Erode-Light.otf',
+      weight: '300',
+      style: "normal"
+    },
+    {
+      path: './Erode/Erode-LightItalic.otf',
+      weight: '300',
+      style: "italic"
+    },
+    {
+      path: './Erode/Erode-Regular.otf',
+      weight: '400',
+      style: "normal"
+    },
+    {
+      path: './Erode/Erode-Italic.otf',
+      weight: "400",
+      style: "italic"
+    },
+    {
+      path: './Erode/Erode-Medium.otf',
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: './Erode/Erode-MediumItalic.otf',
+      weight: "500",
+      style: "italic"
+    },
+    {
+      path: './Erode/Erode-Semibold.otf',
+      weight: "600",
+      style: "normal"
+    },
+    {
+      path: './Erode/Erode-SemiboldItalic.otf',
+      weight: "600",
+      style: "italic"
+    },
+    {
+      path: './Erode/Erode-Bold.otf',
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: './Erode/Erode-BoldItalic.otf',
+      weight: "700",
+      style: "italic",
+    }
+  ],
+  variable: "--font-erode",
+  display: 'block'
+})
+
+export const dm_mono = DM_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '500'],
+})
+
+export const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '700'],
 })
