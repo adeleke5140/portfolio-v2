81af66742383f62af846ae912fd9bc385a71d156
Kenny <adelekekehinde06@gmail.com>
2023-07-05 05:04:04 +0100

    feat:use dm mono variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/themes/laserwave.css";
 import type { AppProps } from "next/app";
-import { satoshiFont, erodeFont } from "@/fonts/setup";
+import { satoshiFont, erodeFont, dm_mono } from "@/fonts/setup";
 import { Layout } from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
-      className={`${satoshiFont.variable} ${erodeFont.variable} home font-sans`}
+      className={`${satoshiFont.variable} ${erodeFont.variable} ${dm_mono.variable} home font-sans`}
       id="container"
     >
       <Layout>
