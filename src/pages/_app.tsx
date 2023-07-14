7117dc1cc00d144d96a2bf546745ecd3529d11ee
Kenny <adelekekehinde06@gmail.com>
2023-07-14 18:30:32 +0100

    feat: add context wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,21 @@ import "@/styles/themes/laserwave.css";
 import type { AppProps } from "next/app";
 import { satoshiFont, erodeFont, dm_mono } from "@/fonts/setup";
 import { Layout } from "@/components/layout";
-
+import { CommandContextWrapper } from "@/context/commandContext";
+import { PostContextWrapper } from "@/context/postContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
       className={`${satoshiFont.variable} ${erodeFont.variable} ${dm_mono.variable} home font-sans`}
       id="container"
     >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CommandContextWrapper>
+        <PostContextWrapper>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PostContextWrapper>
+      </CommandContextWrapper>
     </div>
   );
 }
