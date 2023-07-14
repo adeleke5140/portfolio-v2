db26a8d7e4f5ae0bfba93398c13e1c06f2e1bc8f
Kenny <adelekekehinde06@gmail.com>
2023-07-14 17:51:06 +0100

    feat: add webring attribution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
-import { dm_mono } from "@/fonts/setup";
+import { Home as HomePage } from "@/components/home";
+import { usePostContext } from "@/context/postContext";
+import { getSortedPostsData } from "@/lib/posts";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import { useEffect } from "react";
+import { Post } from "./blog";
+import { A11yFooter } from "@/components/a11yFooter";
 
-export default function Home() {
+export default function Home({
+  allPostsData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { updatePosts } = usePostContext();
+  const completedPosts = allPostsData.filter((post) => {
+    if (post.status !== "draft" && post.status !== "editing") {
+      return post;
+    }
+  });
+  useEffect(() => {
+    updatePosts(completedPosts);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <Head>
@@ -12,45 +29,19 @@ export default function Home() {
         <link rel="icon" href="/kehinde.ico" />
       </Head>
 
-      <div className="max-w-xl pl-8 sm:ml-[12%] sm:pl-16 ">
-        <section className="flex flex-col mt-[43%] justify-center" id="parent">
-          <header className="mb-8">
-            <h1 className="font-satoshi text-heading-color text-6xl sm:text-8xl font-extrabold">
-              Kehinde.
-            </h1>
-            <h2 className="font-mono mt-8 leading-[1.6] font-[300] text-sm">
-              web engineer
-            </h2>
-          </header>
-          <nav>
-            <p>
-              <Link
-                href="/blog"
-                className="inline-block font-mono text-link-color text-2xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-              >
-                /blog.
-              </Link>
-            </p>
-            <p className="mt-4">
-              <Link
-                href="/about"
-                className="inline-block font-mono text-link-color text-2xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-              >
-                /about.
-              </Link>
-            </p>
-            <p className="mt-4">
-              <Link
-                href="/projects"
-                className="inline-block font-mono text-link-color text-2xl opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5"
-              >
-                /projects.
-              </Link>
-            </p>
-          </nav>
-
-        </section>
-      </div>
+      <section className="max-w-[50rem] pl-8 sm:ml-[12%] sm:pl-16">
+        <HomePage />
+        <A11yFooter />
+      </section>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<{ allPostsData: Post[] }> = () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
