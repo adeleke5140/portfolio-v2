378f08c5ad7172987e6a9b6e6a2d61efd54ce5b6
Kenny <adelekekehinde06@gmail.com>
2023-07-14 17:55:19 +0100

    style: improve footer

--- src/components/a11yFooter.tsx
+++ src/components/a11yFooter.tsx
@@ -16,7 +16,7 @@ const A11yFooter = () => {
         <ul className="flex gap-4">
           <li>
             <a
-              className="text-link-color opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
+              className="text-link-color font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
               rel="external"
               referrerpolicy="strict-origin"
               href="https://a11y-webring.club/prev"
@@ -26,7 +26,7 @@ const A11yFooter = () => {
           </li>
           <li>
             <a
-              className="text-link-color opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
+              className="text-link-color font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
               rel="external"
               referrerpolicy="strict-origin"
               href="https://a11y-webring.club/random"
@@ -36,7 +36,7 @@ const A11yFooter = () => {
           </li>
           <li>
             <a
-              className="text-link-color opacity-70 font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
+              className="text-link-color font-extrabold hover:opacity-100 origin-left transition-transform-opacity ease-out duration-200 hover:scale-110 hover:translate-x-1 hover:-translate-y-0.5 focus:outline-none rounded focus:ring focus:ring-heading-color"
               rel="external"
               referrerpolicy="strict-origin"
               href="https://a11y-webring.club/next"
