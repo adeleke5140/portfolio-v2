a34b8b34888f293c82a1002f1f5a2d8d0ffedaad
Kenny <adelekekehinde06@gmail.com>
2023-07-30 14:23:19 +0100

    fix: remove satoshi font

--- src/components/backButton.tsx
+++ src/components/backButton.tsx
@@ -7,7 +7,7 @@ interface BackButtonProps {
 
 const BackButton = ({ path, text }: BackButtonProps) => {
   return (
-    <div className="md:absolute -left-40 top-80 font-satoshi">
+    <div className="md:absolute -left-40 top-80">
       <Link
         href={path}
         className="flex font-mono gap-2 w-fit p-1 px-2 items-center font-extrabold text-button-text md:transition-transform ease-out duration-200 bg-button-bg md:bg-inherit md:hover:bg-button-bg md:-translate-x-1 md:hover:translate-x-0 rounded"
