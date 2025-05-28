71ccbafc42dc85405cb0055d4e102a25d5b6270a
Kenny <adelekekehinde06@gmail.com>
2025-05-28 19:55:42 +0100

    style: enhance layout with background gradients and improve footer styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,15 +5,53 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <div className="flex relative h-full pt-8 pb-8 min-h-screen flex-col ">
-        <main className="pt-10 flex-1 md:pt-12 place-items-center w-full'">
+      <div className="flex relative min-h-screen pt-8 pb-8 flex-col">
+        <div
+          aria-hidden={true}
+          className="absolute top-0 w-[1000px] z-10 h-[400px] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-[0.15] pointer-events-none"
+          style={{
+            background: 'radial-gradient(#e87400, transparent 50%)',
+          }}
+        />
+        <svg
+          className="absolute pointer-events-none inset-0 h-full w-full stroke-gray-200 opacity-50 [mask-image:radial-gradient(100%_100%_at_top_center,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+              width="200"
+              height="200"
+              x="50%"
+              y="-1"
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none"></path>
+            </pattern>
+          </defs>
+          <svg x="50%" y="-1" className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              stroke-width="0"
+            ></path>
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            stroke-width="0"
+            fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+          ></rect>
+        </svg>
+        <main className="pt-10 md:pt-12 place-items-center w-full">
           {children}
         </main>
-        <footer className="w-full mt-10 mb-2 md:px-6 ">
+        <footer className="w-full mt-10 mb-2 md:px-6">
           <div className="max-w-xl rounded-md md:px-6 mx-auto">
             <div className="px-6 py-3 text-[15px] md:px-0 md:pb-2 flex justify-between">
-              <p className="text-sm">ltb.</p>
-              <p className="text-sm">{new Date().getFullYear()}&nbsp;</p>
+              <p className="text-sm font-mono">
+                <span className="text-[#e87400]">©</span>
+                {new Date().getFullYear()}&nbsp;
+              </p>
             </div>
           </div>
         </footer>
