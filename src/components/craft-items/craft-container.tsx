b088aaa1dc7e044548626fcdd2da3cf17617cbe1
Kenny <adelekekehinde06@gmail.com>
2025-06-08 23:22:02 +0100

    feat: allow container take props

--- src/components/craft-items/craft-container.tsx
+++ src/components/craft-items/craft-container.tsx
@@ -1,8 +1,20 @@
+import { cn } from '@/lib/utils'
 import { ReactNode } from 'react'
 
-export const CraftContainer = ({ children }: { children: ReactNode }) => {
+export const CraftContainer = ({
+  children,
+  className,
+}: {
+  children: ReactNode
+  className?: string
+}) => {
   return (
-    <div className="bg-gray-100/50 dark:bg-[#1c1c1c] dark:border-[#282828] border-[0.5px] rounded col-start-1 col-end-4 w-full py-10 p-2">
+    <div
+      className={cn(
+        'bg-gray-100/50 grid place-items-center dark:bg-[#1c1c1c] min-h-[264px] dark:border-[#282828] border-[0.5px] rounded w-full py-10 p-2',
+        className
+      )}
+    >
       {children}
     </div>
   )
