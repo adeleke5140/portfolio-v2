95945b1e869c78a77f686b4bdda0705451c51950
Kenny <adelekekehinde06@gmail.com>
2025-06-12 13:25:27 +0100

    feat: experimenting

--- src/components/home/grid-layout.tsx
+++ src/components/home/grid-layout.tsx
@@ -13,60 +13,44 @@ export const GridLayout = ({
       data-closed={showVariant}
       className="group w-[calc(100%-1.5rem)] top-20 left-[0.8rem] absolute"
     >
-      <div
-        style={{
-          boxShadow: !showVariant
-            ? 'none'
-            : '0px 0px 0px 1px rgba(9, 9, 11, 0.08), 0px 1px 2px -1px rgba(9, 9, 11, 0.08), 0px 2px 4px 0px rgba(9, 9, 11, 0.04)',
-        }}
-        className="bg-white relative top-0 z-30 w-full rounded-xl h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center"
-      >
+      <div className="bg-white border-[0.5px] relative top-0 z-30 w-full rounded-xl h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center">
+        <div className="h-[2.2rem] rounded-b-none w-40 border-[0.5px] border-b-0 grid place-items-center absolute bg-white -top-9 left-8 rounded-xl">
+          Lab
+        </div>
         <motion.button
           layoutId="play"
           onClick={async () => {
+            ;``
             setShowVariant(false)
           }}
           type="button"
-          className="opacity-30 transition-opacity ease-linear hover:opacity-100"
+          className="pt-6 pl-7 origin-center text-black z-30 -left-4 absolute top-[40%] p-2 py-2 rounded font-mono text-xs transition-opacity ease-linear hover:opacity-100"
         >
-          <svg
-            id="Pause"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="w-5 h-5 mx-auto"
-          >
-            <path
-              stroke="#fafafa"
-              d="m9,1H2v1h-1v20h1v1h7v-1h1V2h-1v-1Zm-1,2v18H3V3h5Z"
-            />
-            <path
-              stroke="#fafafa"
-              d="m22,2v-1h-7v1h-1v20h1v1h7v-1h1V2h-1Zm-1,1v18h-5V3h5Z"
-            />
-          </svg>
+          vault
         </motion.button>
+        <div className="absolute w-[calc(100%-1.5rem)] top-2"></div>
       </div>
       <div
         style={
           {
-            '--translate-distance': '-105%',
-            '--scale-value': '0.95',
+            '--translate-distance': '-100%',
+            '--scale-value': '1',
           } as React.CSSProperties
         }
         data-card="card-2"
-        className="card bg-[#fafafa] absolute -translate-y-[100%] z-20 w-full scale-95 stuff rounded-2xl card h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center"
+        className=" bg-[#fafafa] absolute -translate-y-[100%] z-20 w-full scale-95 stuff rounded-2xl h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center"
       >
         some stuff
       </div>
       <div
         style={
           {
-            '--translate-distance': '-110%',
-            '--scale-value': '0.90',
+            '--translate-distance': '-100%',
+            '--scale-value': '1',
           } as React.CSSProperties
         }
         data-card="card-3"
-        className="card bg-[#fafafa] absolute -translate-y-[100%] w-full scale-90 some-stuff z-10 rounded-xl card h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center"
+        className=" bg-[#fafafa] absolute -translate-y-[100%] w-full scale-90 some-stuff z-10 rounded-xl h-[40rem] px-3 py-1 text-sm flex gap-1.5 items-center"
       >
         some other stuff{' '}
       </div>
