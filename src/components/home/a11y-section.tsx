b9097129863555e41aaddf26a6e2ca177b2d781b
Kenny <adelekekehinde06@gmail.com>
2025-06-12 13:25:08 +0100

    feat: remove icons

--- src/components/home/a11y-section.tsx
+++ src/components/home/a11y-section.tsx
@@ -2,7 +2,7 @@ import { ArrowUpRight } from 'lucide-react'
 
 export const A11ySection = () => {
   return (
-    <div className="bg-[#fafafa] w-full card rounded-xl  px-3 py-1 text-sm flex gap-1.5 items-center h-20">
+    <div className="bg-white w-full rounded-2xl rounded-tl-sm  px-3 py-1 text-sm flex gap-1.5 items-center h-20">
       <nav className="flex flex-col gap-2" aria-labelledby="a11y-webring-club">
         <p>
           This site is a member of the{' '}
@@ -18,47 +18,32 @@ export const A11ySection = () => {
         <ul className="flex  gap-2">
           <li className="">
             <a
-              className=" bg-gray-100 card justify-between group flex gap-1.5 text-[15px] items-center px-3 py-1 rounded-2xl"
+              className=" bg-gray-100 justify-between group flex gap-1.5 text-[15px] items-center px-3 py-1 rounded-xl"
               rel="external"
               referrerPolicy="strict-origin"
               href="https://a11y-webring.club/prev"
             >
               Previous
-              <ArrowUpRight
-                size={12}
-                aria-hidden
-                className="group-hover:-translate-y-0.5 group-hover:translate-x-0.5 transition-transform"
-              />
             </a>
           </li>
           <li className="">
             <a
-              className=" bg-gray-100 card justify-between group px-3 flex gap-1.5 items-center text-[15px] py-1 rounded-2xl"
+              className=" bg-gray-100 justify-between group px-3 flex gap-1.5 items-center text-[15px] py-1 rounded-xl"
               rel="external"
               referrerPolicy="strict-origin"
               href="https://a11y-webring.club/random"
             >
               Random
-              <ArrowUpRight
-                size={12}
-                aria-hidden
-                className="group-hover:-translate-y-0.5 group-hover:translate-x-0.5 transition-transform"
-              />
             </a>
           </li>
           <li className="">
             <a
-              className=" bg-gray-100 card justify-between group flex gap-1.5 items-center px-3 text-[15px] py-1 rounded-2xl"
+              className=" bg-gray-100 justify-between group flex gap-1.5 items-center px-3 text-[15px] py-1 rounded-xl"
               rel="external"
               referrerPolicy="strict-origin"
               href="https://a11y-webring.club/next"
             >
               Next
-              <ArrowUpRight
-                size={12}
-                aria-hidden
-                className="group-hover:-translate-y-0.5 group-hover:translate-x-0.5 transition-transform"
-              />
             </a>
           </li>
         </ul>
