6dada71664c4909b0d908cfe1f4a5fc2453fd02d
Kenny <adelekekehinde06@gmail.com>
2025-06-12 13:25:39 +0100

    feat: more experiments

--- src/components/home/now-playing.tsx
+++ src/components/home/now-playing.tsx
@@ -11,15 +11,10 @@ export const NowPlayingSection = ({
 }) => {
   const [isPlaying, setIsPlaying] = useState(false)
   return (
-    <motion.div layoutId="wrapper" className="w-1/4 h-20">
+    <motion.div layoutId="wrapper" className="w-1/4 h-10">
       <div
-        style={{
-          boxShadow: showVariant
-            ? 'none'
-            : '0px 0px 0px 1px rgba(9, 9, 11, 0.08), 0px 1px 2px -1px rgba(9, 9, 11, 0.08), 0px 2px 4px 0px rgba(9, 9, 11, 0.04)',
-        }}
         className={cn(
-          'bg-white rounded-xl  transition-shadow group overflow-hidden relative flex justify-center items-center text-sm gap-1.5 w-full h-full font-mono'
+          'bg-gray-100 rounded-xl rounded-bl-sm  transition-shadow group overflow-hidden relative flex justify-center items-center text-sm gap-1.5 w-full h-full font-mono'
         )}
       >
         <motion.button
@@ -29,18 +24,9 @@ export const NowPlayingSection = ({
             setShowVariant(true)
           }}
           type="button"
+          className="inset-0 group/play absolute inline-flex items-center gap-1 justify-center"
         >
-          <svg
-            id="play"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="w-5 h-5 mx-auto"
-          >
-            <path
-              stroke="#fafafa"
-              d="m21,11v-1h-1v-1h-2v-1h-2v-1h-1v-1h-2v-1h-2v-1h-1v-1h-2v-1h-2v-1h-3v1h-1v20h1v1h3v-1h2v-1h2v-1h1v-1h2v-1h2v-1h1v-1h2v-1h2v-1h1v-1h1v-2h-1Zm-2,2h-2v1h-2v1h-1v1h-2v1h-2v1h-1v1h-2v1h-2v1h-1V3h1v1h2v1h2v1h1v1h2v1h2v1h1v1h2v1h2v2Z"
-            />
-          </svg>
+          <span>Lab</span>
         </motion.button>
       </div>
     </motion.div>
