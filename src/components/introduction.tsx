7cfa8c15e30c1647259a001cb7aadb6cb69e1851
Kenny <adelekekehinde06@gmail.com>
2025-06-12 16:41:43 +0100

    feat: update desc again

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -38,9 +38,8 @@ export const Introduction = () => {
         <div className="flex flex-col text-[var(--gray-1)] text-[15px] gap-2 dark:border-[#282828] dark:bg-[#282828]">
           <div className="rounded-lg text-[15px] dark:text-[#fefefe]">
             <p>
-              Hi, I&apos;m Kenny. I am web craftsman so I care about the
-              products that I build. My craft is very dear to me. I currently
-              work at{' '}
+              Hi, I&apos;m Kenny. I am web craftsman with interest in typefaces,
+              design and motion. I currently work at{' '}
               <a
                 href="https://www.mastra.ai"
                 className="border-b border-[#e87400]   text-[#e87400]"
@@ -52,9 +51,9 @@ export const Introduction = () => {
 
           <div>
             <p className="dark:text-[#fefefe] text-[15px] rounded-lg h-full">
-              I am drawn to design because of beauty. Beauty compels people to
-              use products. One thing I think about often is beautiful and
-              joyful design can be used to tell stories.
+              I am drawn to design because of beauty. One thing I think about
+              often is how beautiful and joyful design can be used to tell
+              stories.
             </p>
           </div>
         </div>
