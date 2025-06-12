8a3ec90d7d553011cee74913c0c0dc27eaa474a1
Kenny <adelekekehinde06@gmail.com>
2025-06-12 13:24:31 +0100

    feat: extend footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -43,13 +43,50 @@ const Layout = ({ children }: LayoutProps) => {
           ></rect>
         </svg>
         <main className="pt-10 md:pt-12  w-full">{children}</main>
-        <footer className="w-full mt-10 mb-2 md:px-6">
-          <div className="max-w-xl rounded-md md:px-6 mx-auto">
-            <div className="px-6 py-3 text-[15px] md:px-0 md:pb-2 flex justify-between">
-              <p className="text-sm font-mono">
+        <footer className="w-full mt-10 mb-2 px-6">
+          <div className="max-w-xl rounded-md md:px-6 flex justify-between items-center mx-auto">
+            <div className="py-1 px-2 bg-white rounded-xl text-[15px] flex justify-between">
+              <p className="text-sm inline-flex items-center gap-1">
                 <span className="text-[#e87400]">©</span>
-                {new Date().getFullYear()}&nbsp;
+                <span>{new Date().getFullYear()}&nbsp;</span>
               </p>
+            </div>
+            <div className=" text-[15px] bg-gray-100 w-fit rounded-xl py-1 px-2  dark:text-[#fefefe]">
+              <a className="flex items-center gap-1" href="mailto:k@kehinde.me">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  className="w-4 h-4 text-[#e87400]"
+                >
+                  <title>envelope</title>
+                  <g fill="currentColor">
+                    <path
+                      d="m3,7l6.504,3.716c.307.176.685.176.992,0l6.504-3.716"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    ></path>
+                    <rect
+                      x="3"
+                      y="4"
+                      width="14"
+                      height="12"
+                      rx="3"
+                      ry="3"
+                      fill="none"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></rect>
+                  </g>
+                </svg>
+                <span>Mail</span>
+              </a>
             </div>
           </div>
         </footer>
