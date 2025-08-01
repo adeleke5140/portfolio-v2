997cdf448e591c68bdd6105d4c897900302de35b
Kenny <adelekekehinde06@gmail.com>
2025-08-01 22:00:48 +0100

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import '../styles/globals.css'
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center mt-[20%]">
-      <h1 className="text-7xl font-inter font-bold">404</h1>
-      <p className="text-xl">Page not found!</p>
+      <h1 className="text-9xl font-serif leading-[1.6] font-bold">404</h1>
+      <p className="text-xl -mt-8">Page not found!</p>
       <Link href="/" className="underline text-[var(--primary)]">
         Go Home
       </Link>
