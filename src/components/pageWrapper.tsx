2c257a33c1655696cd184fcaa50195937e908235
Kenny <adelekekehinde06@gmail.com>
2025-08-01 22:01:44 +0100

    Update heading styles and container padding

--- src/components/pageWrapper.tsx
+++ src/components/pageWrapper.tsx
@@ -24,14 +24,14 @@ const PageWrapper = ({
 }: PageWrapperProps) => {
   const slot =
     typeof heading == 'string' ? (
-      <h1 className="font-inter mb-10 capitalize font-semibold leading-[100%] tracking-[-0.96px] text-[48px]">
+      <h1 className="font-serif py-10 lg:text-7xl font-medium w-full border-b-[0.5px]  border-b-[#dcdcdc] mb-10 capitalize leading-[100%] tracking-[-0.96px] text-[48px]">
         {heading}
       </h1>
     ) : (
       heading
     )
   return (
-    <div className={cn('relative px-6 max-w-[680px] mx-auto', classname)}>
+    <div className={cn('relative px-6 md:px-0 mx-auto', classname)}>
       <div className="flex flex-col gap-5">
         {showLink ? (
           <BackButton path={path ? path : ''} text={`${backText}`} />
