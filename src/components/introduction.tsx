fe74d0c25669a8b665e962149574532f90926886
Kenny <adelekekehinde06@gmail.com>
2025-08-24 14:51:47 +0100

    feat: add connection

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -3,6 +3,25 @@ import { PageWrapper } from '@/components/pageWrapper'
 import React from 'react'
 import { A11ySection } from './home/a11y-section'
 
+const connection = [
+  {
+    name: 'email',
+    value: 'mailto:k@kehinde.me',
+  },
+  {
+    name: 'twitter',
+    value: 'https://twitter.com/adeleke5140',
+  },
+  {
+    name: 'github',
+    value: 'https://github.com/adeleke5140',
+  },
+  {
+    name: 'linkedin',
+    value: 'https://www.linkedin.com/in/adeleke5140/',
+  },
+]
+
 export const Introduction = () => {
   const TITLE = 'Design Engineer'
   return (
@@ -55,13 +74,9 @@ export const Introduction = () => {
         </section>
 
         <section className="flex flex-col gap-5">
-          <p className="font-medium text-[17px]">Currently Building:</p>
+          <p className="font-medium text-[17px]">Currently Building</p>
           <div>
             <a
-              // style={{
-              //   boxShadow:
-              //     '0px 0px 0px 1px #0000000f,0px 1px 2px -1px #0000000f,0px 2px 4px 0px #0000000a',
-              // }}
               target="_blank"
               href="https://chromewebstore.google.com/detail/nihongo-speech/jhojfellcdlldagfemimnjebnfoodchf?authuser=0&hl=en-GB"
               className="bg-white inline-block py-2 px-2 rounded-xl w-full"
@@ -75,6 +90,23 @@ export const Introduction = () => {
                 </span>
               </span>
             </a>
+          </div>
+        </section>
+
+        <section className="flex flex-col gap-5">
+          <p className="font-medium text-[17px]">Connect</p>
+          <div className="flex gap-2">
+            {connection.map((con) => (
+              <a
+                target="_blank"
+                href={con.value}
+                className="capitalize bg-gray-100 justify-between group flex gap-1.5 text-[15px] items-center px-3 py-1 rounded-xl"
+                rel="external"
+                referrerPolicy="strict-origin"
+              >
+                {con.name}
+              </a>
+            ))}
           </div>
         </section>
 
