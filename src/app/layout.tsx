8e4e1a4a5fa06a5b5062f08c4ea041c7820d0194
Kenny <adelekekehinde06@gmail.com>
2025-11-18 13:37:02 +0100

    feat: move position of nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,20 @@
 import { fonts } from '@/fonts/setup'
 import '@/styles/globals.css'
-import Link from 'next/link'
 import { Toaster } from 'sonner'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${fonts.mondwest.variable} ${fonts.berkeleyMono.variable} ${fonts.neue.variable} ${fonts.editorialNew.variable}`}
+      >
         <div
-          className={`min-h-screen font-sans max-w-[1028px] mx-auto ${fonts.mondwest.variable} ${fonts.berkeleyMono.variable} ${fonts.neue.variable} ${fonts.editorialNew.variable} bg-[rgb(248,249,250)] text-ken-black`}
+          className={`min-h-screen font-sans max-w-[1028px] mx-auto  bg-[rgb(248,249,250)] text-ken-black`}
           id="container"
         >
           <Toaster />
           <div className="relative flex flex-col min-h-screen max-w-[1280px]  mx-auto pb-8">
-            <nav className="flex cursor-pointer w-fit ml-auto gap-4 px-3 md:px-0 h-full items-center py-5">
-              <Link href="/craft">Craft</Link>
-              <Link href="/blog">Blog</Link>
-              <Link href="/">Home</Link>
-            </nav>
-            <main className="flex-1 w-full">{children}</main>
+            <main className="flex-1 mt-16 w-full">{children}</main>
           </div>
         </div>
       </body>
