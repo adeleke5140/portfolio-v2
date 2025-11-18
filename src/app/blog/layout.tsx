d7ddd7cf570ae20c4397c9b6826748def9684481
Kenny <adelekekehinde06@gmail.com>
2025-11-18 20:51:17 +0100

    feat: create scaffolding for assistant types

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import { KenAssistant } from '@/components/blog/ken-assistant'
-import { ToggleKen } from '@/components/blog/toggle-ken'
+import { KenAssistant } from '@/components/blog/assistant'
+import { ToggleAssistant } from '@/components/blog/toggle-assistant'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 
@@ -10,18 +16,56 @@ export default function BlogLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [isMaximized, setIsMaximized] = useState(false)
+  return (
+    <div>
+      {children}
+      <div className="bg-white w-[400px] flex flex-col justify-end fixed top-0 bottom-0 right-0 h-full">
+        <KenAssistant
+          isOpen={true}
+          onClose={() => {}}
+          isMaximized={false}
+          setIsMaximized={() => {}}
+        />
+      </div>
+      {/* <AssistantPopover /> */}
+    </div>
+  )
+}
+
+function AssistantPopover() {
   const [isChatOpen, setIsChatOpen] = useState(false)
   const pathname = usePathname()
+  const [isMaximized, setIsMaximized] = useState(false)
 
   return (
-    <>
-      {children}
-      <ToggleKen onClick={() => setIsChatOpen(true)} />
-      <KenAssistant
-        key={pathname}
-        isOpen={isChatOpen}
-        onClose={() => setIsChatOpen(false)}
-      />
-    </>
+    <div className={cn('fixed bottom-6 rounded-xl right-6 z-30')}>
+      <Popover open={isChatOpen} onOpenChange={setIsChatOpen}>
+        <PopoverTrigger onClick={() => setIsChatOpen(true)}>
+          <ToggleAssistant />
+        </PopoverTrigger>
+        <PopoverContent
+          side="top"
+          className="w-fit shadow-none p-0 mr-2 rounded-none border-none bg-transparent"
+        >
+          <div
+            className={cn(
+              'h-[500px] bottom-2 z-50',
+              'w-full md:w-[450px]',
+              'bg-[rgb(248,249,250)]  border border-gray-200',
+              'flex flex-col rounded-t-[20px] rounded-b-[40px] shadow-2xl'
+            )}
+          >
+            <KenAssistant
+              key={pathname}
+              isOpen={isChatOpen}
+              onClose={() => setIsChatOpen(false)}
+              isMaximized={isMaximized}
+              setIsMaximized={setIsMaximized}
+            />
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
