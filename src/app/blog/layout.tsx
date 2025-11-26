82c130d7ae77ba0d521645477a9e68f077046c1e
Kenny <adelekekehinde06@gmail.com>
2025-11-26 16:10:58 +0100

    feat: move ken assistant to root

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,25 +1,7 @@
-import { BlogAssistantWrapper } from '@/components/blog/blog-assistant-wrapper'
-import { getSortedPostsData } from '@/lib/posts'
-
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  // Fetch recent articles on the server
-  const posts = getSortedPostsData()
-  const recentArticles = posts
-    .slice(-10)
-    .reverse()
-    .map((post) => ({
-      id: post.id,
-      title: (post as any).title!,
-    }))
-
-  return (
-    <div>
-      {children}
-      <BlogAssistantWrapper recentArticles={recentArticles} />
-    </div>
-  )
+  return <div>{children}</div>
 }
