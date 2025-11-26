5fffe774f4292bf9b71fb5cb6d6d1d8810add4a7
Kenny <adelekekehinde06@gmail.com>
2025-11-26 18:11:22 +0100

    feat: use jotai atoms

--- src/components/blog/assistant-context.tsx
+++ src/components/blog/assistant-context.tsx
@@ -1,56 +1,7 @@
 'use client'
 
-import {
-  createContext,
-  useContext,
-  useState,
-  type Dispatch,
-  type ReactNode,
-  type SetStateAction,
-} from 'react'
+import { atom } from 'jotai'
 
-type BlogAssistantContextValue = {
-  isMaximized: boolean
-  setIsMaximized: Dispatch<SetStateAction<boolean>>
-  isOpen: boolean
-  setIsOpen: Dispatch<SetStateAction<boolean>>
-  isChatOpen: boolean
-  setIsChatOpen: Dispatch<SetStateAction<boolean>>
-}
-
-const BlogAssistantContext = createContext<
-  BlogAssistantContextValue | undefined
->(undefined)
-
-export function BlogAssistantProvider({ children }: { children: ReactNode }) {
-  const [isMaximized, setIsMaximized] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
-  const [isChatOpen, setIsChatOpen] = useState(false)
-
-  return (
-    <BlogAssistantContext.Provider
-      value={{
-        isMaximized,
-        setIsMaximized,
-        isOpen,
-        setIsOpen,
-        isChatOpen,
-        setIsChatOpen,
-      }}
-    >
-      {children}
-    </BlogAssistantContext.Provider>
-  )
-}
-
-export function useBlogAssistant() {
-  const context = useContext(BlogAssistantContext)
-
-  if (context === undefined) {
-    throw new Error(
-      'useBlogAssistant must be used within a BlogAssistantProvider'
-    )
-  }
-
-  return context
-}
+export const maximizedAtom = atom(false)
+export const isOpenAtom = atom(false)
+export const isChatOpenAtom = atom(false)
