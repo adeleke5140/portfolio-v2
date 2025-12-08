dc5c72ef763ae8ceeefacbd1cc4036e0353d7f14
Kenny <adelekekehinde06@gmail.com>
2025-12-08 15:00:22 +0100

    fix: cookies error

--- src/components/blog/blog-assistant-wrapper.tsx
+++ src/components/blog/blog-assistant-wrapper.tsx
@@ -1,8 +1,5 @@
-import { getInitialMessages } from '@/lib/chat'
 import { BlogAssistantClient } from './blog-assistant-client'
 
 export async function BlogAssistantWrapper() {
-  const initialMessages = await getInitialMessages()
-
-  return <BlogAssistantClient initialMessages={initialMessages} />
+  return <BlogAssistantClient initialMessages={[]} />
 }
