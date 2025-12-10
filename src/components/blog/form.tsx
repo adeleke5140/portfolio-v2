5742d33c7e347a531f6e681e295088cfa02f4f0a
Kenny <adelekekehinde06@gmail.com>
2025-12-10 13:15:56 +0100

    refactor: use usageAlert component

--- src/components/blog/form.tsx
+++ src/components/blog/form.tsx
@@ -1,6 +1,6 @@
 import { cn, transFormDashToSpaceCase } from '@/lib/utils'
 import { Loader } from '../ai-elements/loader'
-import { BlogIcon } from '../craft/navigation/navigation'
+import { UsageAlert } from './usage-alert'
 interface FormProps {
   input: string
   setInput: (input: string) => void
@@ -10,31 +10,6 @@ interface FormProps {
   context: string
   recentArticles?: Array<{ id: string; title: string }>
   rateLimitRemaining: number
-  rateLimitError: string
-}
-const isDev = process.env.NODE_ENV === 'development'
-function UsageAlert({
-  rateLimitRemaining,
-  rateLimitError,
-}: {
-  rateLimitRemaining: number
-  rateLimitError: string
-}) {
-  if (isDev) return null
-  return (
-    <div className="border mx-5 border-b-0 border-[#dcdcdc] h-8 pt-2 rounded-t-xl bg-inherit p-4">
-      {rateLimitRemaining === 0 ? (
-        <p className="text-red-700 rounded-lg font-sans text-xs">
-          {rateLimitError}
-        </p>
-      ) : (
-        <p className="text-[12px] text-xs font-sans text-ken-grey">
-          {rateLimitRemaining}{' '}
-          {rateLimitRemaining === 1 ? 'message' : 'messages'} remaining today
-        </p>
-      )}
-    </div>
-  )
 }
 
 export const Form = ({
@@ -45,10 +20,9 @@ export const Form = ({
   textareaRef,
   context,
   rateLimitRemaining,
-  rateLimitError,
 }: FormProps) => {
   const customErrorForTextArea =
-    process.env.NODE_ENV === 'production' && rateLimitError
+    process.env.NODE_ENV === 'production' && rateLimitRemaining === 0
       ? 'We can chat again in 24hours...'
       : null
   return (
@@ -61,23 +35,13 @@ export const Form = ({
       }}
       className="p-4 pt-0 relative"
     >
-      <UsageAlert
-        rateLimitRemaining={rateLimitRemaining}
-        rateLimitError={rateLimitError}
-      />
+      <UsageAlert rateLimitRemaining={rateLimitRemaining} />
       <div className="relative">
         <div
           className={cn(
             'flex flex-col bg-white focus-visible:outline focus-visible:outline-[var(--primary)] shadow-[0px_10px_24px_-6px_#0000001a,0px_2px_4px_-1px_#0000000f,0_0_0_1px_#54483114] gap-2 px-4 py-2.5 h-32 rounded-[1.25rem] border-r-0'
           )}
         >
-          <div className="text-center text-gray-500 text-sm">
-            <p className="border flex font-sans items-center gap-1 border-[#dcdcdc]/50 bg-gray-100/50 rounded-full text-xs px-[10px] py-[5px] text-left w-fit">
-              <BlogIcon className="size-3" />
-              {context}
-            </p>
-          </div>
-
           <textarea
             ref={textareaRef}
             value={input || ''}
@@ -99,7 +63,7 @@ export const Form = ({
                 ? "You can ask about what I've written."
                 : `Ask Kenny about ${transFormDashToSpaceCase(context)}`
             }
-            className="w-full font-sans bg-white h-full outline-none  disabled:bg-inherit resize-none disabled:cursor-not-allowed text-sm"
+            className="w-full font-sans pt-1 bg-white h-full outline-none  disabled:bg-inherit resize-none disabled:cursor-not-allowed text-sm"
           />
         </div>
 
