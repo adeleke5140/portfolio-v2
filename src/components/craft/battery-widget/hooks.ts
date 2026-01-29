1e56e2b4cee2cf8e124074185fdb6634eecec4e5
Kenny <adelekekehinde06@gmail.com>
2026-01-29 14:38:41 +0100

    chore: linting

--- src/components/craft/battery-widget/hooks.ts
+++ src/components/craft/battery-widget/hooks.ts
@@ -1,44 +1,43 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from 'react'
 
 interface BatteryManager {
-  charging: boolean;
-  chargingTime: number;
-  dischargingTime: number;
-  level: number;
-  onchargingchange: () => any;
-  onchargingtimechange: () => any;
-  ondischargingtimechange: () => any;
+  charging: boolean
+  chargingTime: number
+  dischargingTime: number
+  level: number
+  onchargingchange: () => any
+  onchargingtimechange: () => any
+  ondischargingtimechange: () => any
   onlevelchange: () => any
 }
-interface INavigator extends Navigator{
-  getBattery:() => Promise<BatteryManager>
+interface INavigator extends Navigator {
+  getBattery: () => Promise<BatteryManager>
 }
 
 interface Battery {
-  percentage: number;
-  charging: boolean;
+  percentage: number
+  charging: boolean
 }
 
+function useBattery() {
+  const [battery, setBattery] = useState<Battery>({
+    percentage: 0.69,
+    charging: false,
+  })
 
-function useBattery(){
-  const [battery, setBattery] = useState<Battery>(
-    { percentage: 0.69, charging: false }
-  )
-  
-  useEffect(() =>{
-    if(!window) return
-   
-    async function getBattery(){
+  useEffect(() => {
+    if (!window) return
+
+    async function getBattery() {
       const n = window.navigator as INavigator
-      if("getBattery" in n){
+      if ('getBattery' in n) {
         const battery = await n.getBattery()
-        setBattery({percentage: battery.level, charging: battery.charging})
-      }else{
-        setBattery({percentage: 0.69, charging: false})
+        setBattery({ percentage: battery.level, charging: battery.charging })
+      } else {
+        setBattery({ percentage: 0.69, charging: false })
       }
     }
     getBattery()
-  },[])
+  }, [])
   return battery
 }
