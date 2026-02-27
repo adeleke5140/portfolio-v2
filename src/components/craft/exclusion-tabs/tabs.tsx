b94d0fe70e615857e360fc7c5e1788f7d9889d90
Kenny <adelekekehinde06@gmail.com>
2026-02-27 18:11:10 +0100

    fix: change active color

--- src/components/craft/exclusion-tabs/tabs.tsx
+++ src/components/craft/exclusion-tabs/tabs.tsx
@@ -61,7 +61,7 @@ export function Tabs() {
       </ul>
 
       <div aria-hidden className="clip-path-container" ref={containerRef}>
-        <ul className="relative list-none flex w-full gap-2 max-w-[40rem] flex-wrap justify-start bg-primary">
+        <ul className="relative list-none flex w-full gap-2 max-w-[40rem] flex-wrap justify-start bg-blue-500">
           {TABS.map((tab) => (
             <li key={tab.name}>
               <button
